0b4bae9e0d09c6500d514f63703fe85757fda835
GamerIndia <155443275+GamerIndiaOfficial@users.noreply.github.com>
2024-12-20 19:38:51 +0530

    Update main.py
    Updated main.py to add some comments and make it more optimised by my new python knowledge

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from datetime import date
-exit = "YES"
-current_year = date.today().year
-while exit == "yes" or exit == "Yes" or exit == "YES":
+exit = "YES" #Yes by default so user can get in loop
+current_year = date.today().year #Getting Current Year
+while exit == "YES": #Puts user in a loop
  print("This is a simple age calculator")
- birth_year = int(input("What year were you born?"))
- age = current_year - birth_year
+ birth_year = int(input("What year were you born?")) #Stores User Birth Year In a Variable
+ age = current_year - birth_year #Subtract Current Year by Birth Year to get age
  print("Your age is", age)
- exit = input("Do you want to use age finder again? ")
-if exit == "no" or exit == "No" or exit == "NO":
+ exit = input("Do you want to use age finder again? ").upper() #Asks user if want to exit
+if exit == "NO":
   print("Program ended successfully ")
 else:
   print("Invalid input enter yes or no")
